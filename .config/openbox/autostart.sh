bc3aa8a57c9ed3a73656d51cfa1c79503a64b3da
Ivan Kanakarakis <ivan.kanak@gmail.com>
2010-12-13 18:15:13 +0200

    update ob entries

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -7,3 +7,8 @@ tint2 &
 # start parcellite clipboard
 parcellite &
 
+# have a termnal ready
+$HOME/.bin/urxvtc -geometry 98x28+54+52 & #90x24+54+52
+
+# nowplaying conky
+sleep 2 && conky -c $HOME/.conky/nowplaying.conkyrc &
