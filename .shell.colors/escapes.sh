cf5dab1106597f560c6ad757532212978bef6361
Ivan Kanakarakis <ivan.kanak@gmail.com>
2013-10-23 21:24:58 +0300

    add shell colors

---
+++ .shell.colors/escapes.sh
@@ -0,0 +1,41 @@
+fgblk="\e[0;30m"        # Black - Regular
+fgred="\e[0;31m"        # Red
+fggrn="\e[0;32m"        # Green
+fgylw="\e[0;33m"        # Yellow
+fgblu="\e[0;34m"        # Blue
+fgpur="\e[0;35m"        # Purple
+fgcyn="\e[0;36m"        # Cyan
+fgwht="\e[0;37m"        # White
+
+bfgblk="\e[1;30m"       # Black - Bright
+bfgred="\e[1;31m"       # Red
+bfggrn="\e[1;32m"       # Green
+bfgylw="\e[1;33m"       # Yellow
+bfgblu="\e[1;34m"       # Blue
+bfgpur="\e[1;35m"       # Purple
+bfgcyn="\e[1;36m"       # Cyan
+bfgwht="\e[1;37m"       # White
+
+bgblk="\e[0;40m"        # Black - Background
+bgred="\e[0;41m"        # Red
+bggrn="\e[0;42m"        # Green
+bgylw="\e[0;43m"        # Yellow
+bgblu="\e[0;44m"        # Blue
+bgpur="\e[0;45m"        # Purple
+bgcyn="\e[0;46m"        # Cyan
+bgwht="\e[0;47m"        # White
+
+bbgblk="\e[1;40m"       # Black - Background - Bright
+bbgred="\e[1;41m"       # Red
+bbggrn="\e[1;42m"       # Green
+bbgylw="\e[1;43m"       # Yellow
+bbgblu="\e[1;44m"       # Blue
+bbgpur="\e[1;45m"       # Purple
+bbgcyn="\e[1;46m"       # Cyan
+bbgwht="\e[1;47m"       # White
+
+normal="\e[0m"          # text reset
+mkbold="\e[1m"          # make bold
+undrln="\e[4m"          # underline
+mkblnk="\e[5m"          # make blink
+revers="\e[7m"          # reverse
